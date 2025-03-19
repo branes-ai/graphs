bef1a0e5ac3ab836f41b8a37296aa0395868d643
Ravenwater <theo@stillwater-sc.com>
2025-03-19 19:44:25 -0400

    adding mobilenet V2

---
+++ workloads/pytorch/mobilenet_V2/mobilenet.py
@@ -0,0 +1,20 @@
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+from PIL import Image
+import requests
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+preprocessor = AutoImageProcessor.from_pretrained("google/mobilenet_v2_1.0_224")
+model = AutoModelForImageClassification.from_pretrained("google/mobilenet_v2_1.0_224")
+
+inputs = preprocessor(images=image, return_tensors="pt")
+
+outputs = model(**inputs)
+logits = outputs.logits
+
+# model predicts one of the 1000 ImageNet classes
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
+
+# Note: This model actually predicts 1001 classes, the 1000 classes from ImageNet plus an extra “background” class (index 0).
