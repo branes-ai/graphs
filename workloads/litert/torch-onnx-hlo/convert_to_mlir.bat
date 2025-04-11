fec00ce6d69210bcc0d76650849f618d1a2452a1
Kishore KONDAPI <kishore.kondapi@branes.ai>
2025-04-11 14:50:56 -0400

    Modified after onnx-mlir, onnx-mlir-opt builds

--- workloads/litert/torch-onnx-hlo/convert_to_mlir.bat
+++ workloads/litert/torch-onnx-hlo/convert_to_mlir.bat
@@ -1,5 +1,8 @@
 @echo off
-iree-import-onnx efficientnet_b0.onnx -o mlir/efficientnet_b0_onnx.mlir
-iree-import-onnx efficientnet_b0.onnx -o mlir/efficientnet_b0_stablehlo.mlir
+REM #iree-import-onnx efficientnet_b0.onnx -o mlir/efficientnet_b0_onnx.mlir
+REM #iree-import-onnx efficientnet_b0.onnx -o mlir/efficientnet_b0_stablehlo.mlir
+onnx-mlir efficientnet_b0_opset13.onnx --EmitONNXIR -o efficientnet_b0_opset13_onnx
+REM #onnx-mlir efficientnet_b0_opset13_onnx.mlir --EmitMLIR -o efficientnet_b0_stablehlo.mlir
+onnx-mlir-opt efficientnet_b0_opset13_onnx.onnx.mlir --convert-onnx-to-stablehlo -o efficientnet_b0_stablehlo.mlir
 echo Done converting to ONNX and StableHLO MLIR
 pause
